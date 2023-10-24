5736353a6b979c39045c3edcfb31af1ee71e9f85
Igor Zhirkov <iz@matterlabs.dev>
2023-10-24 18:45:08 +0200

    minimal backend generates encoder

---
+++ llvm/lib/Target/Bogus/MCTargetDesc/BogusMCCodeEmitter.cpp
@@ -0,0 +1,53 @@
+#include "BogusMCCodeEmitter.h"
+#include "MCTargetDesc/BogusMCTargetDesc.h"
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCFixup.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrDesc.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/EndianStream.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+#include <cassert>
+#include <cstdint>
+
+using namespace llvm;
+
+#define DEBUG_TYPE "mccodeemitter"
+
+#define GET_INSTRMAP_INFO
+#include "BogusGenInstrInfo.inc"
+#undef GET_INSTRMAP_INFO
+
+
+void BogusMCCodeEmitter::EmitByte(unsigned char C, raw_ostream &OS) const {
+  OS << (char)C;
+}
+
+/// encodeInstruction - Emit the instruction.
+/// Size the instruction with Desc.getSize().
+void BogusMCCodeEmitter::encodeInstruction(const MCInst &MI,
+                                          SmallVectorImpl<char> &CB,
+                                          SmallVectorImpl<MCFixup> &Fixups,
+                                          const MCSubtargetInfo &STI) const {
+  // Non-pseudo instructions that get changed for direct object
+  // only based on operand values.
+  // If this list of instructions get much longer we will move
+  // the check to a function call. Until then, this is more efficient.
+  MCInst TmpInst = MI;
+
+  uint32_t Binary = getBinaryCodeForInstr(TmpInst, Fixups, STI);
+  //  const MCInstrDesc &Desc = MCII.get(TmpInst.getOpcode());
+  support::endian::write<uint32_t>(CB, Binary, llvm::endianness::big);
+}
+
+#if 0
+#include "BogusGenCodeEmitter.inc"
+#endif
---
+++ llvm/lib/Target/Bogus/MCTargetDesc/BogusMCCodeEmitter.h
@@ -0,0 +1,47 @@
+#ifndef LLVM_LIB_TARGET_BOGUS_MCTARGETDESC_BOGUSMCCODEEMITTER_H
+#define LLVM_LIB_TARGET_BOGUS_MCTARGETDESC_BOGUSMCCODEEMITTER_H
+
+#include "llvm/MC/MCCodeEmitter.h"
+#include <cstdint>
+
+namespace llvm {
+
+class MCContext;
+class MCExpr;
+class MCFixup;
+class MCInst;
+class MCInstrInfo;
+class MCOperand;
+class MCSubtargetInfo;
+class raw_ostream;
+
+class BogusMCCodeEmitter : public MCCodeEmitter {
+  const MCInstrInfo &MCII;
+  MCContext &Ctx;
+
+public:
+  BogusMCCodeEmitter(const MCInstrInfo &mcii, MCContext &Ctx)
+      : MCII(mcii), Ctx(Ctx) {}
+
+  BogusMCCodeEmitter(const BogusMCCodeEmitter &) = delete;
+  BogusMCCodeEmitter &operator=(const BogusMCCodeEmitter &) = delete;
+  ~BogusMCCodeEmitter() override = default;
+
+  void EmitByte(unsigned char C, raw_ostream &OS) const;
+
+  void encodeInstruction(const MCInst &MI, SmallVectorImpl<char> &CB,
+                         SmallVectorImpl<MCFixup> &Fixups,
+                         const MCSubtargetInfo &STI) const override;
+
+  // getBinaryCodeForInstr - TableGen'erated function for getting the
+  // binary encoding for an instruction.
+  uint64_t getBinaryCodeForInstr(const MCInst &MI,
+                                 SmallVectorImpl<MCFixup> &Fixups,
+                                 const MCSubtargetInfo &STI) const;
+
+
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_BOGUS_MCTARGETDESC_BOGUSMCCODEEMITTER_H
